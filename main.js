84740d47cf303628298e4bcd0902c5a84795023e
0908jw <ljw010908jw@gmail.com>
2024-10-15 19:26:56 -0400

    commit 1

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 
     $('#toggle').click(function() {
         if (isToggled) {
-            $('#background-container').css('background-image', 'url(https://cdn.mos.cms.futurecdn.net/nT6eb3GNFQJ6AerZkWVqBo-320-80.gif)'); 
+            $('#background-container').css({'background-image': 'url(https://cdn.mos.cms.futurecdn.net/nT6eb3GNFQJ6AerZkWVqBo-320-80.gif)',
+            'filter': 'none'}); 
             $('#hello').css({
                 'color': 'whitesmoke', 
                 'text-shadow': '1px 1px 10px #fff, 1px 1px 10px #ccc'
@@ -13,14 +14,10 @@ $(document).ready(function() {
         } else {
 
             $('#background-container').css({
-                'background-image': 'url(./image/whiteversion.gif)',
+                'filter': 'invert(1)',
                 'background-size': 'cover',         
                 'background-position': 'center',    
                 'background-repeat': 'no-repeat'    
-            });
-            $('#hello').css({
-                'color': '#000000',
-                'text-shadow': '2px 2px 20px #000000, 2px 2px 20px #000000'
             });
 
             $('#toggle').text('Dark mode');
